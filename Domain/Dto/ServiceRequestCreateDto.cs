7d5ae9c7373237714f569806824117c7122aa907
Irina Centeno <irycente@gmail.com>
2021-09-09 16:00:45 -0300

    Added DTOs.

---
+++ Domain/Dto/ServiceRequestCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Dto
+{
+    /// <summary>
+    /// Assumed that this are the fields that the user needs to provide to create a new Service Request. Normally I'd validate with BA if not in User Story.
+    /// </summary>
+    public class ServiceRequestCreateDto
+    {
+        public string BuildingCode { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ Domain/Dto/ServiceRequestUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Dto
+{
+    /// <summary>
+    /// Assumed that this are the fields that the user needs to provide to create a new Service Request. Normally I'd validate with BA if not in User Story.
+    /// </summary>
+    public class ServiceRequestUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string BuildingCode { get; set; }
+        public string Description { get; set; }
+        public int? CurrentStatus { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
