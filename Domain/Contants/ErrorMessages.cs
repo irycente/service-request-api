a54d4185074d80fc6f38077199bb0cb8ec6568fc
Irina Centeno <irycente@gmail.com>
2021-09-09 17:23:04 -0300

    Added controller.

--- Domain/Contants/ErrorMessages.cs
+++ Domain/Contants/ErrorMessages.cs
@@ -4,5 +4,6 @@
     {
         public const string RESOURCE_NOT_FOUND = "The resource requested could not be found.";
         public const string NO_RESOURCES_AVAILABLE = "There are no resources available.";
+        public const string EMPTY_REQUEST = "Empty request.";
     }
 }
---
+++ ServiceRequestApi/Controllers/ServiceRequestsController.cs
@@ -0,0 +1,120 @@
+﻿using Business.ServiceRequests;
+using Domain.Contants;
+using Domain.Dto;
+using Domain.Entities;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace ServiceRequestApi.Controllers
+{
+    [Route("api/servicerequest")]
+    public class ServiceRequestsController : ControllerBase
+    {
+        private readonly IServiceRequestRepository repository;
+
+        public ServiceRequestsController(IServiceRequestRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<ServiceRequest>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var requests = repository.GetAll();
+
+                return Ok(requests);
+            }
+            catch (NoResourcesAvailableException)
+            {
+                return NoContent();                
+            };
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ServiceRequest), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                var request = repository.GetById(id);
+
+                return Ok(request);
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ServiceRequest), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create([FromBody]ServiceRequestCreateDto requestDto)
+        {
+            if (requestDto == null)
+            {
+                return BadRequest(ErrorMessages.EMPTY_REQUEST);
+            }
+
+            // TODO: Apply business validations.
+
+            var newRequest = repository.Create(requestDto);
+
+            return CreatedAtAction(nameof(GetById), newRequest.Id, newRequest);
+        }
+
+        [HttpPut]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Update([FromBody] ServiceRequestUpdateDto requestDto)
+        {
+            if (requestDto == null)
+            {
+                return BadRequest(ErrorMessages.EMPTY_REQUEST);
+            }
+
+            try
+            {
+                // TODO: Apply business validations.
+
+                repository.Update(requestDto);
+
+                return Ok();
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                repository.Delete(id);
+
+                return Ok();
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                return NotFound(ex);
+            }
+        }
+    }
+}
