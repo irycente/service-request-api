7b704f82996da0b34e5bf3f80a8aca8775bcdb83
Irina Centeno <irycente@gmail.com>
2021-09-09 16:11:45 -0300

    Added repository interface and implementation.

---
+++ Business/ServiceRequests/IServiceRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Dto;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Business.ServiceRequests
+{
+    public interface IServiceRequestRepository
+    {
+        IEnumerable<ServiceRequest> GetAll();
+        ServiceRequest GetById(Guid id);
+        ServiceRequest Create(ServiceRequestCreateDto newRequestInfo);
+        void Update(ServiceRequestUpdateDto requestUpdate);
+        void Delete(Guid id);
+    }
+}
---
+++ DataAccess/ServiceRequests/Repositories/ServiceRequestMockRepository.cs
@@ -0,0 +1,43 @@
+﻿using Business.ServiceRequests;
+using Domain.Dto;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.ServiceRequests.Repositories
+{
+    public class ServiceRequestMockRepository : IServiceRequestRepository
+    {
+        private readonly List<ServiceRequest> serviceRequests;
+
+        public ServiceRequestMockRepository(List<ServiceRequest> mockedServiceRequests)
+        {
+            serviceRequests = mockedServiceRequests;
+        }
+
+        public ServiceRequest Create(ServiceRequestCreateDto newRequestInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ServiceRequest> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceRequest GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ServiceRequestUpdateDto requestUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
